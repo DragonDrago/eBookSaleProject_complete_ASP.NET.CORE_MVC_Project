5a02dc36aa85141680ee6c3347602cc1103bbc8a
DragonDrago <akobirmuso@gmail.com>
2022-01-28 04:18:42 +0300

    Add project files.

---
+++ eBookSaleProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eBookSaleProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
